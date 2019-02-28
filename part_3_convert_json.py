30d3ba595d67f1bdfff36bddf77332b7a2a09af5
Valerie Wang <34406500+ValerieWang628@users.noreply.github.com>
2019-02-27 21:40:48 -0500

    Add files via upload

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -2,42 +2,7 @@ import cc_dat_utils
 import json
 import cc_data
 
-# the yunziw_cc1.json file only listed three very basic optional fields
-# advanced fields, like monsters, and buttons, etc are written through the python functions, not manually scripted in json
-# because it is a little bit easier to specify object attributes than typing lists/objects in json
-
 #Part 3
-def add_trap(ccLevel):
-    '''
-    this helper function adds trap to each level
-    '''
-    if ccLevel.level_number == 1:
-        ccTrapCoordinates = cc_data.CCTrapControl(6,6,2,3)
-    elif ccLevel.level_number == 2:
-        ccTrapCoordinates = cc_data.CCTrapControl(6,6,7,3)
-    return ccTrapCoordinates
-
-def add_clone_machine(ccLevel):
-    '''
-    this helper function adds cloning machine to each level
-    '''
-    if ccLevel.level_number == 1:
-        ccCloneCoordinates = cc_data.CCCloningMachineControl(2,7,5,7)
-    elif ccLevel.level_number == 2:
-        ccCloneCoordinates = cc_data.CCCloningMachineControl(3,7,5,7)
-    return ccCloneCoordinates
-
-def activate_monsters(ccLevel):
-    '''
-    this helper function adds monster movement to each level
-    '''
-    if ccLevel.level_number == 1:
-        ccMonsterCoordinates = [cc_data.CCCoordinate(5,3),cc_data.CCCoordinate(5,4),cc_data.CCCoordinate(6,4)]
-    elif ccLevel.level_number == 2:
-        ccMonsterCoordinates = [cc_data.CCCoordinate(8,3),cc_data.CCCoordinate(8,4), cc_data.CCCoordinate(8,5),
-                                cc_data.CCCoordinate(8,6)]
-    return ccMonsterCoordinates
-
 
 def load_json_to_CCDataFile(json_data):
     # ccDataFile is a container to hold all levels
@@ -57,28 +22,35 @@ def load_json_to_CCDataFile(json_data):
         ccLevel.lower_layer = item["level"]["lower_layer"]
 
         # title, password, and hint are three basic fields that 
-        ccMapTitleField = cc_data.CCMapTitleField(item["level"]["optional_fields"][0]["field_3"]["title"])
+        ccMapTitleField = cc_data.CCMapTitleField(item["level"]["optional_fields"]["field_3"]["title"])
         ccLevel.add_field(ccMapTitleField)
-        ccEncodedPasswordField = cc_data.CCEncodedPasswordField(item["level"]["optional_fields"][1]["field_6"]["password"])
+        ccEncodedPasswordField = cc_data.CCEncodedPasswordField(item["level"]["optional_fields"]["field_6"]["password"])
         ccLevel.add_field(ccEncodedPasswordField)
-        ccMapHintField = cc_data.CCMapHintField(item["level"]["optional_fields"][2]["field_7"]["hint"])
+        ccMapHintField = cc_data.CCMapHintField(item["level"]["optional_fields"]["field_7"]["hint"])
         ccLevel.add_field(ccMapHintField)
-        
-        # adding moving monsters
-        ccMonsterCoordinates = activate_monsters(ccLevel)
-        ccMonsterMovementField = cc_data.CCMonsterMovementField(ccMonsterCoordinates)
+
+        # adding monsters can also be
+        ccMonsterMovementField = []
+        for monster in item["level"]["optional_fields"]["field_10"]["monsters"]:
+            ccMonsterMovementField.append(cc_data.CCCoordinate(monster[0],monster[1]))
+        ccMonsterMovementField = cc_data.CCMonsterMovementField(ccMonsterMovementField)
         ccLevel.add_field(ccMonsterMovementField)
 
         # adding trap sets
-        ccTrapCoordinates = add_trap(ccLevel)
-        ccTrapControlsField = cc_data.CCTrapControlsField([ccTrapCoordinates])
+        ccTrapCoordinates = []
+        for trap in item["level"]["optional_fields"]["field_4"]["traps"]:
+            ccTrapCoordinates.append(cc_data.CCTrapControl(trap[0], trap[1], trap[2], trap[3]))
+        ccTrapControlsField = cc_data.CCTrapControlsField(ccTrapCoordinates)
         ccLevel.add_field(ccTrapControlsField)
 
         # adding cloning machines
-        ccCloningCoordinates = add_clone_machine(ccLevel)
-        ccCloningMachineFields = cc_data.CCCloningMachineControlsField([ccCloningCoordinates])
-        ccLevel.add_field(ccCloningMachineFields)
+        ccCloningMachines = []
+        for machine in item["level"]["optional_fields"]["field_5"]["cloning machines"]:
+            ccCloningMachines.append(cc_data.CCCloningMachineControl(machine[0], machine[1], machine[2], machine[3]))
+        ccCloningMachineField = cc_data.CCCloningMachineControlsField(ccCloningMachines)
+        ccLevel.add_field(ccCloningMachineField)
 
+        # adding levels into the ccDataFile Object
         ccDataFile.levels.append(ccLevel)
     
     return ccDataFile
